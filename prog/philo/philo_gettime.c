a0c094981b54cbedc9f47396cdcafd5ab7bca91b
Wicca <Selletrer@mail.ru>
2022-05-01 19:17:33 +0300

    fix: gettime
    free tv

--- prog/philo/philo_gettime.c
+++ prog/philo/philo_gettime.c
@@ -18,13 +18,14 @@ long int	ft_gettime_simul_start(void)
 	struct timeval	*tv;
 
 	tv = (struct timeval *)malloc(sizeof(struct timeval) * 1);
-	if (!gettimeofday(tv, NULL))
+	if (tv != NULL && !gettimeofday(tv, NULL))
 	{
 		timestap_in_mcs = tv->tv_sec * 1000000 + tv->tv_usec;
 		free(tv);
-		tv = NULL;
 		return (timestap_in_mcs);
 	}
+	if (tv != NULL)
+		free(tv);
 	write(STDERR_FILENO, "Error: gettimeofday error\n", 26);
 	return (-1);
 }
